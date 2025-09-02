354c0053b8023d34db19f9fb7a1d000002d350a9
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-09-02 23:55:53 +0800

    ✨ Add Vercel Analytics integration and update app.vue template

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLoadingIndicator />
+  <Analytics />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script lang="ts" setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
